eaba9c4ddbc84b64d20e1db9e0b725a937e6d39e
Joseph Esteban <joseph.esteban@fractalservicios.pe>
2017-08-23 17:58:46 -0500

    update

--- src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -1,11 +1,15 @@
 package com.udemy.backendninja.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.udemy.backendninja.model.Person;
 
 @Controller
 @RequestMapping("/example")
@@ -16,7 +20,7 @@ public class ExampleController {
 	@GetMapping("/exampleString")
 	public String exampleString(Model model) 
 	{
-		model.addAttribute("name", "Jhon");
+		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
 	
@@ -24,7 +28,20 @@ public class ExampleController {
 	public ModelAndView exampleMaV()
 	{
 		ModelAndView wav = new ModelAndView(EXAMPLE_VIEW);
-		wav.addObject("name", "Mikel");
+		wav.addObject("person", new Person("Fiorella", 29));
 		return wav;
+	}
+	
+	private List<Person> getPeople()
+	{
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Jhon", 24));
+		people.add(new Person("Eva", 14));
+		people.add(new Person("Ana", 44));
+		people.add(new Person("Petter", 14));
+		people.add(new Person("Fiorella", 44));
+		people.add(new Person("Saori", 54));
+		
+		return people;
 	}
 }
